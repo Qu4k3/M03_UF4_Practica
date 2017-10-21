f7c57cb70e9e6262bde07c9ebae7ac88dd1f8937
h2jie <h2jie09@gmail.com>
2017-10-21 21:25:43 +0200

    clase completa

--- src/Modelo/verdura/Seta.java
+++ src/Modelo/verdura/Seta.java
@@ -2,15 +2,20 @@ package Modelo.verdura;
 
 public class Seta extends Verdura {
 
-    public Seta(String nombreProducto, String nombreMarca, int unidades, boolean mantenerFresco, String fechaAdd) {
-        super(nombreProducto, nombreMarca, unidades, mantenerFresco, fechaAdd);
+    public Seta(String nombreProducto, String nombreMarca, int unidades, boolean conservarFresco, String fechaAdd, double precioPorKilo, double peso, String origen) {
+        super(nombreProducto, nombreMarca, unidades, conservarFresco, fechaAdd, precioPorKilo, peso, origen);
     }
-
 
 
     @Override
     public double totalPrecio(int unidades) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return precioPorKilo*unidades;
+    }
+    
+        @Override
+    public String toString() {
+        return "SinAlcohol{ nombre: " + nombreProducto + "marca: " + nombreMarca + "unidad: "
+                + unidades + "mantener fresco? " + conservarFresco + "fecha: " + fechaAdd + "precio: " + precioPorKilo + "peso: " + peso + "}";
     }
     
 }
--- src/Modelo/verdura/Verdura.java
+++ src/Modelo/verdura/Verdura.java
@@ -3,13 +3,16 @@ package Modelo.verdura;
 import Modelo.Producto;
 
 public abstract class Verdura extends Producto {
-    
+
+    protected double precioPorKilo;
+    protected double peso;
     protected String origen;
 
-    public Verdura(String nombreProducto, String nombreMarca, int unidades, boolean mantenerFresco, String fechaAdd) {
-        super(nombreProducto, nombreMarca, unidades, mantenerFresco, fechaAdd);
+    public Verdura(String nombreProducto, String nombreMarca, int unidades, boolean conservarFresco, String fechaAdd, double precioPorKilo, double peso, String origen) {
+        super(nombreProducto, nombreMarca, unidades, conservarFresco, fechaAdd);
+        this.precioPorKilo = precioPorKilo;
+        this.peso=peso;
+        this.origen = origen;
     }
 
-    
-    
 }
